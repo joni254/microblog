ee937b908a931caf15f391a870e060ee21cb61a8
john <jmwangi288@gmail.com>
2021-05-03 09:48:46 +0300

    3.5.2021

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 from flask.helpers import url_for
 from flask_login.utils import login_required, logout_user
-from app.forms import LoginForm, RegistrationForm
+from flask_wtf import form
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask import render_template, flash, redirect, request
 from app import app, db
 from flask_login import current_user, login_user, logout_user
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 @app.route('/')
 @app.route('/register', methods = ['GET','POST'])
@@ -21,6 +23,14 @@ def register():
         flash("Congratulations you are now a registered user!")
         return redirect(url_for('login'))
     return render_template('register.html', title = 'Register', form = form)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
 @app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
@@ -58,6 +68,22 @@ def user(username):
         {'author': user, 'body':'Test post 2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET','POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved successfully')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
 
 @app.route('/logout')
 def logout():
--- config.py
+++ config.py
@@ -4,7 +4,13 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a very long key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'postgresql://mwangi:.D3v3l0p3r!@localhost/myblog'
-    DEBUG = True
-     
+    DEBUG = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int( os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['john@citorium.com']
+         
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     
